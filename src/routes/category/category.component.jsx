4519d88bcc1704af4e593f87c9dc86992c61a654
Aaron Graham <aarongraham@Aarons-Mac-mini.lan>
2023-08-02 19:03:02 -0400

    fixed a few style clashes

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -16,11 +16,14 @@ export const Category = () => {
   }, [category, categoriesMap]);
 
   return (
-    <div className="category-container">
+    <>    
+    <h2 className="category-title">{category.toUpperCase()}</h2>
+    <div className="category-container">      
       {products &&
         products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
     </div>
+    </>
   );
 };
